ddaa8ca815ef86faecb520a5876244cfeb8de128
Terry <t3rrygong@gmail.com>
2020-03-28 00:47:59 -0600

    added function to print all the local storage events and change text area backgroud color according to the time

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
-var today = moment().format("dddd");
-var date = moment().format("MMMM Do YYYY");
-
 var eventList = ["", "", "", "", "", "", "", "", ""];
 
 //display current date
-$("#currentDay").text(today + ", " + date);
+$("#currentDay").text(moment().format("dddd") + ", " + moment().format("MMMM Do YYYY"));
+
+//display any localstorage event and change text area background color
+printEventList();
 
 //when click save button, store user input to the evenlist array
 $(".btn").on("click", function () {
@@ -12,5 +12,37 @@ $(".btn").on("click", function () {
     var inputArea = "[value=inputArea" + $(this).val() + "]";
     var input = $(inputArea).val().trim();
     eventList[currentTimeBlock] = input;
-    console.log(eventList);
+    localStorage.setItem("event", JSON.stringify(eventList));
 });
+
+function printEventList() {
+    var currentHour = moment().hour();
+
+    //if no localstorage of "event" then don't print
+    if (JSON.parse(localStorage.getItem("event")) !== null) {
+        eventList = JSON.parse(localStorage.getItem("event"));
+        for (let i = 0; i < 9; i++) {
+            var inputArea = "[value=inputArea" + i + "]";
+            $(inputArea).val(eventList[i]);
+        }
+    }
+
+    //change the text area background color according to the time
+    if (currentHour < 9) {
+        $(".form-control").addClass("bg-success");
+    } else if (currentHour > 17) {
+        $(".form-control").addClass("bg-secondary");
+    } else {
+        currentHour = currentHour - 9;
+        var currentHourTextArea = "[value=inputArea" + currentHour + "]";
+        $(currentHourTextArea).removeClass("bg-success");
+        $(currentHourTextArea).addClass("bg-danger");
+        if (currentHour > 0) {
+            for (let i = 0; i < currentHour; i++) {
+                var pastTextArea = "[value=inputArea" + i + "]";
+                $(pastTextArea).removeClass("bg-success");
+                $(pastTextArea).addClass("bg-secondary");
+            }
+        }
+    }
+}
