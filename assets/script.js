adaa40a116eba06f967c612d073cc199f2524307
Terry <t3rrygong@gmail.com>
2020-03-27 23:58:34 -0600

    I can click save button and store user input to an array now

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,16 @@
 var today = moment().format("dddd");
 var date = moment().format("MMMM Do YYYY");
 
+var eventList = ["", "", "", "", "", "", "", "", ""];
+
+//display current date
 $("#currentDay").text(today + ", " + date);
+
+//when click save button, store user input to the evenlist array
+$(".btn").on("click", function () {
+    var currentTimeBlock = $(this).val();
+    var inputArea = "[value=inputArea" + $(this).val() + "]";
+    var input = $(inputArea).val().trim();
+    eventList[currentTimeBlock] = input;
+    console.log(eventList);
+});
